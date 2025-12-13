d46c79a5a782e2017a91a1fc65fcc51978d75bb0
kyle-parker-1500 <kylerparker1500@gmail.com>
2025-12-13 03:21:22 -0800

    got dropdown to populate with filters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from filters import apply_effect, EFFECT_LIST, API_KEY
 
 # create instance of Flask
 app = Flask(__name__)
@@ -9,7 +10,10 @@ bootstrap = Bootstrap5(app)
 # route decorator binds a function to a URL
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        effect_list=EFFECT_LIST
+        )
 
 @app.route('/import_image')
 def images():
