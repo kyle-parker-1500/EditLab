73b1d217020caace42da08e971e767706486171f
kyle-parker-1500 <kylerparker1500@gmail.com>
2025-12-14 22:44:22 -0800

    all chroma keys work now

--- chroma.py
+++ chroma.py
@@ -51,20 +51,33 @@ def reverse_chroma_key(foreground_path, target_color, threshold=60):
     return result
 
 # ---- KYLE FUNCTIONS TO HANDLE REPLACING PIXELS WITH IMAGE ----
-def color_distance(pixel, color):
-    return (
-        (pixel[0] - color[0])**2 +
-        (pixel[1] - color[1])**2 +
-        (pixel[2] - color[2])**2        
-    )
+def chroma_key_with_img(foreground_path, background_path, color, threshold=100):
+    src = Image.open(foreground_path).convert("RGB") 
+    bg = Image.open(background_path).convert("RGB") 
 
-def chroma_key_with_img(src, bg, color, threshold=100):
-    for x in range(src.width):
-       for y in range(src.height):
-           cur_pixel = src.getpixel((x,y))
+    # resize images to match
+    if src.size != bg.size:
+        bg = bg.resize(src.size, Image.LANCZOS)
+        
+    result = src.convert("RGBA")
+    pixels_result = result.load()
+    pixels_bg = bg.load()
+    
+    for x in range(result.width):
+       for y in range(result.height):
+            r, g, b, a = pixels_result[x, y] 
+            cur_pixel = (r, g, b)
 
-           if color_distance(cur_pixel, color) < threshold:
-               src.putpixel((x,y), bg.getpixel((x,y)))
-      
-    return src
+            # get distance
+            distance = (
+                (cur_pixel[0] - color[0])**2 +
+                (cur_pixel[1] - color[1])**2 +
+                (cur_pixel[2] - color[2])**2        
+            )
+
+            if distance < threshold * threshold:
+                bg_r, bg_g, bg_b = pixels_bg[x, y]
+                pixels_result[x, y] = (bg_r, bg_g, bg_b, 255)
+     
+    return result 
 
